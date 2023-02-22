d658b244a6f27485775e055c7f214c9e24afe46e
MustafaAldelaimi <mustafa1007@yahoo.com>
2023-02-22 14:18:44 +0000

    Part3 complete

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
-
+app.use(express.json())
 const port = 3000;
 
 //TODO
@@ -10,6 +10,26 @@ const port = 3000;
 app.get('/musicians/:id', async (req, res) => {
     const musicians = await Musician.findByPk(req.params.id);
     res.json(musicians);
+})
+
+app.post('/musicians', async (req, res) => {
+    const musician = await Musician.create(req.body);
+    res.send(musician);
+})
+
+app.put('/musicians/:id', async (req, res) => {
+    const musician = await Musician.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.json(musician);
+})
+
+app.delete('/musicians/:id', async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    musician.destroy();
+    res.json(musician);
 })
 
 app.listen(port, () => {
