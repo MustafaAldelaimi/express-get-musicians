4e463e895674e7b1adf7b166bd17ea333b18118d
MustafaAldelaimi <mustafa1007@yahoo.com>
2023-02-23 15:52:33 +0000

    part 5 complete

--- routes/musicians.js
+++ routes/musicians.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {Musician} = require("../Musician")
-const {sequelize} = require("../db")
+const {check, validationResult} = require("express-validator");
 router.use(express.json())
 
 //TODO
@@ -16,7 +16,16 @@ router.get('/', async (req, res) => {
           res.json(musicians);
 })
       
-router.post('/', async (req, res) => {
+router.post('/', [
+          check('name', 'Name is required').not().isEmpty().trim(),
+          check('instrument', 'Instrument is required').not().isEmpty().trim(),
+          check('name', 'Too long or short').isLength({min: 2, max: 20}),
+], 
+          async (req, res) => {
+          const errors = validationResult(req);
+          if (!errors.isEmpty()) {
+              return res.send({errors: errors.array()});
+          }
           const musician = await Musician.create(req.body);
           res.send(musician);
 })
